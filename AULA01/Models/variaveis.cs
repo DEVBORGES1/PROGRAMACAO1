e869f6829fac8e2ea56b13edc669357347fe1fd4
403392 <403392@VDA-LCOII-19158>
2025-02-24 22:05:38 -0300

    max e min value

--- AULA01/Models/variaveis.cs
+++ AULA01/Models/variaveis.cs
@@ -7,6 +7,17 @@
         
         // Anotação de tipos
         public int userCount = 10;
+        public byte MinValue = 0;
+        public byte MaxValue = 255;
+        public short MinshortValue = -32768;
+        public short MaxshortValue = 32767;
+        public int MinintValue = -2147483648;
+        public int MaxintValue = 2147483647;
+        public uint MinuintValue = 0;
+        public uint MaxuintValue = 4294967295;
+        public long MinlongValue = -9223372036854775808;
+        public long MaxlongValue = 9223372036854775807;
+
 
         // Uma variavel pode ser declarada 
         // e não inicializada 
--- AULA01/obj/Debug/net9.0/AULA01.AssemblyInfo.cs
+++ AULA01/obj/Debug/net9.0/AULA01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AULA01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23ae1a6b919cc3ba0f3145e4368bd91a01eea8ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f0c0ed8726b0620f0e6ff4f03b778692ac7adf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AULA01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AULA01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
